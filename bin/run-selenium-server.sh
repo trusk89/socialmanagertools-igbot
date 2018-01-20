5b2f4ee35a398d7aa826e59f5c3fa622e26daea2
Patryk Rzucidło <ptkdev@gmail.com>
2018-01-20 10:57:28 +0100

    [Fix] Selenium error: java heap space

--- bin/run-selenium-server.sh
+++ bin/run-selenium-server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xvfb-run --server-args='-screen 0, 1920x1080x16' java -Dwebdriver.chrome.driver="./bin/chromedriver" -jar ./bin/selenium-server-standalone.jar
+xvfb-run --server-args='-screen 0, 1920x1080x16' java -Xms512m -Xmx2048m -Dwebdriver.chrome.driver="./bin/chromedriver" -jar ./bin/selenium-server-standalone.jar
