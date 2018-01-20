eae543ec15f0e7be88b12e6f32935c4b884688f1
Patryk Rzucidło <ptkdev@gmail.com>
2018-01-20 11:00:33 +0100

    [Fix] bad args position

--- bin/run-selenium-desktop.sh
+++ bin/run-selenium-desktop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Dwebdriver.chrome.driver="./bin/chromedriver" -jar -Xms512m -Xmx2048m ./bin/selenium-server-standalone.jar
+java -Xms512m -Xmx2048m -Dwebdriver.chrome.driver="./bin/chromedriver" -jar ./bin/selenium-server-standalone.jar
