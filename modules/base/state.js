38e7e23146b4099fc55460f262ba2c074b65fc06
agoalofalife <agoalofalife@gmail.com>
2018-03-27 11:26:40 +0300

    create state interface add state interface in comment mode module

---
+++ modules/base/state.js
@@ -0,0 +1,80 @@
+// Export module
+const EventEmitter = require('events').EventEmitter;
+
+/**
+ * Type statuses
+ * @type {{OK: number, ERROR: number, READY: number, START: null}}
+ */
+const STATE = {
+    OK: 1,
+    ERROR: 0,
+    READY: 3,
+    START:null
+};
+const EVENTS = {
+    CHANGE_STATUS:'change_status'
+};
+/**
+ *
+ * @type {module.State}
+ */
+class ManagerState extends EventEmitter{
+    constructor(params) {
+        super(params);
+        this._status = STATE.START;
+        this.registerHandler();
+    }
+
+    /**
+     * register handle events in EE
+     */
+    registerHandler(){
+        this.on(EVENTS.CHANGE_STATUS, (status) => {
+            this._status = status;
+        });
+    }
+
+    /**
+     * Get current status
+     * @return STATE
+     */
+    getStatus(){
+        return this._status;
+    }
+
+    /**
+     * Check is ready status
+     * @return {boolean}
+     */
+    isReady(){
+        return this._status === STATE.READY;
+    }
+
+    /**
+     * Check is 'ok' status
+     * @return {boolean}
+     */
+    isOk(){
+        return this._status === STATE.OK;
+    }
+    /**
+     * Check is 'error' status
+     * @return {boolean}
+     */
+    isError(){
+        return this._status === STATE.ERROR;
+    }
+    /**
+     * Check is 'start' status
+     * @return {boolean}
+     */
+    isStart(){
+        return this._status === STATE.START;
+    }
+}
+
+module.exports = {
+  STATE:STATE,
+  EVENTS:EVENTS,
+  ManagerState:ManagerState
+};
--- modules/commentmode_classic.js
+++ modules/commentmode_classic.js
@@ -1,6 +1,10 @@
+// Export
 const LOG = require('../modules/logger/types');
 const LOG_NAME = 'comment';
-const LOG_MODE = 'commentmode';
+const LOG_MODE = 'comment_mode';
+const ManagerState = require('../modules/base/state').ManagerState;
+const STATE = require('../modules/base/state').STATE;
+const STATE_EVENTS = require('../modules/base/state').EVENTS;
 
 /**
  * MODE: commentmode_classic
@@ -12,16 +16,13 @@ const LOG_MODE = 'commentmode';
  * @changelog:  0.1 initial release
  *
  */
-class CommentMode_classic{
+class CommentMode_classic extends ManagerState{
     constructor(bot, config, utils) {
+        super();
+
         this.bot = bot;
         this.config = config;
         this.utils = utils;
-        this.status = {
-            OK: 1,
-            ERROR: 0,
-            CURRENT: null,
-        };
     }
 
     /**
@@ -41,6 +42,7 @@ class CommentMode_classic{
     async like_open_hashtagpage() {
         let hashtag_tag = this.config.instagram_hashtag[Math.floor(Math.random() * this.config.instagram_hashtag.length)];
         this.utils.logger(LOG.INFO, LOG_NAME, "current hashtag " + hashtag_tag);
+
         try {
             await this.bot.goto('https://www.instagram.com/explore/tags/' + hashtag_tag + '/');
         } catch (err) {
@@ -71,7 +73,7 @@ class CommentMode_classic{
 
                 this.utils.sleep(this.utils.random_interval(10, 15));
 
-                if (this.config.debug == true)
+                if (this.config.debug === true)
                     this.utils.logger(LOG.DEBUG, LOG_NAME, "array photos " + cache_hashtag);
                 do {
                     photo_url = cache_hashtag.pop();
@@ -119,18 +121,19 @@ class CommentMode_classic{
     async comment() {
         this.utils.logger(LOG.INFO, LOG_NAME, "try leave comment");
 
-        let texterea = "";
+        let texterea = '';
         let commentAreaElem = 'main article:nth-child(1) section:nth-child(5) form textarea';
         let nickUnderPhoto = `main article:nth-child(1) div:nth-child(3) div:nth-child(3) ul li a[title="${this.config.instagram_username}"]`;
+
         try {
             texterea = await this.bot.$(commentAreaElem);
-            if (texterea != null) {
-                this.status.CURRENT = this.status.OK;
+            if (texterea !== null) {
+                this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.OK);
             } else {
-                this.status.CURRENT = this.status.ERROR;
+                this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.ERROR);
             }
 
-            if (this.status.CURRENT === this.status.OK) {
+            if (this.isOk()) {
                 await this.bot.waitForSelector(commentAreaElem);
                 let button = await this.bot.$(commentAreaElem);
                 await button.click();
@@ -138,13 +141,13 @@ class CommentMode_classic{
                 await button.press('Enter');
             } else {
                 this.utils.logger(LOG.INFO, LOG_NAME, "bot is unable to comment on this photo");
-                this.status.CURRENT = this.status.ERROR;
+                this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.ERROR);
             }
         } catch (err) {
             if (this.config.debug === true)
                 this.utils.logger(LOG.DEBUG, LOG_NAME, err);
             this.utils.logger(LOG.INFO, LOG_NAME, "bot is unable to comment on this photo");
-            this.status.CURRENT = this.status.ERROR;
+            this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.ERROR);
         }
 
         this.utils.sleep(this.utils.random_interval(4, 8));
@@ -157,40 +160,37 @@ class CommentMode_classic{
 
         this.utils.sleep(this.utils.random_interval(4, 8));
 
-        if (this.status.CURRENT === this.status.OK) {
+        if (this.isOk()) {
             try {
                 texterea = await this.bot.$(nickUnderPhoto);
 
                 if (texterea != null) {
-                    this.status.CURRENT = this.status.OK;
+                    this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.OK);
                 } else {
-                    this.status.CURRENT = this.status.ERROR;
+                    this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.ERROR);
                 }
 
-                if (this.status.CURRENT == this.status.ERROR) {
+                if (this.isError()) {
                     this.utils.logger(LOG.WARNING, LOG_NAME, "</3");
                     this.utils.logger(LOG.WARNING, LOG_NAME, "error bot :( not comment under photo, now bot sleep 5-10min");
                     this.utils.logger(LOG.WARNING, LOG_NAME, "You are in possible soft ban... If this message appear all time stop bot for 24h...");
                     this.utils.sleep(this.utils.random_interval(60 * 5, 60 * 10));
-                } else if (this.status.CURRENT == this.status.OK) {
+                } else if (this.isOk()) {
                     this.utils.logger(LOG.INFO, LOG_NAME, "<3");
                 }
             } catch (err) {
-                if (this.config.debug == true)
+                if (this.config.debug === true)
                     this.utils.logger(LOG.DEBUG, LOG_NAME, err);
-                this.status.CURRENT = this.status.ERROR;
+                this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.ERROR);;
             }
         } else {
             this.utils.logger(LOG.WARNING, LOG_NAME, "</3");
             this.utils.logger(LOG.WARNING, LOG_NAME, "You like this previously, change hashtag ig have few photos");
-            this.status.CURRENT = 3;
+            this.emit(STATE_EVENTS.CHANGE_STATUS, STATE.READY);
         }
 
         this.utils.sleep(this.utils.random_interval(2, 5));
-
         await this.utils.screenshot(LOG_NAME, "last_comment_after");
-
-        return this.status.CURRENT;
     }
 
     /**
@@ -213,12 +213,12 @@ class CommentMode_classic{
             let hour = today.getHours() + "" + (today.getMinutes() < 10 ? '0' : '');
             let minutes = today.getMinutes();
 
-            this.utils.logger(LOG.INFO, "comment_mode", `time night: ${hour}:${minutes}`);
+            this.utils.logger(LOG.INFO, LOG_MODE, `time night: ${hour}:${minutes}`);
 
             if (parseInt(hour + minutes) >= (this.config.bot_sleep_night).replace(":", "")) {
                 t1 = new Date(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours(), today.getMinutes(), today.getSeconds());
-                this.utils.logger(LOG.INFO, "comment_mode", "loading... " + new Date(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours(), today.getMinutes(), today.getSeconds()));
-                this.utils.logger(LOG.INFO, "comment_mode", "cache array size " + cache_hashtag.length);
+                this.utils.logger(LOG.INFO, LOG_MODE, "loading... " + new Date(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours(), today.getMinutes(), today.getSeconds()));
+                this.utils.logger(LOG.INFO, LOG_MODE, "cache array size " + cache_hashtag.length);
                 if (cache_hashtag.length <= 0)
                     await this.like_open_hashtagpage();
 
@@ -228,18 +228,18 @@ class CommentMode_classic{
 
                 this.utils.sleep(this.utils.random_interval(4, 8));
 
-                like_status = await this.comment();
+                await this.comment();
 
-                if (cache_hashtag.length < 9 || like_status == 3) //remove popular photos
+                if (cache_hashtag.length < 9 || this.isReady()) //remove popular photos
                     cache_hashtag = [];
 
-                if (cache_hashtag.length <= 0 && like_status != 3) {
-                    this.utils.logger(LOG.INFO, "comment_mode", "finish fast comment, bot sleep " + this.config.bot_fastlike_min + "-" + this.config.bot_fastlike_max + " minutes");
+                if (cache_hashtag.length <= 0 && this.isReady()) {
+                    this.utils.logger(LOG.INFO, LOG_MODE, "finish fast comment, bot sleep " + this.config.bot_fastlike_min + "-" + this.config.bot_fastlike_max + " minutes");
                     cache_hashtag = [];
                     this.utils.sleep(this.utils.random_interval(60 * this.config.bot_fastlike_min, 60 * this.config.bot_fastlike_max));
                 }
             } else {
-                this.utils.logger(LOG.INFO, "comment_mode", "is night, bot sleep");
+                this.utils.logger(LOG.INFO, LOG_MODE, "is night, bot sleep");
                 this.utils.sleep(this.utils.random_interval(60 * 4, 60 * 5));
             }
         } while (true);
